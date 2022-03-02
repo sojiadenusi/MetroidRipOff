2eb56973b1060b0c65c2686a0a99ee68b4ebf94e
sojiadenusi <43679596+sojiadenusi@users.noreply.github.com>
2022-03-01 22:57:59 -0500

    stuff

--- MetroidRip/Assets/Scripts/MovingPlatform.cs
+++ MetroidRip/Assets/Scripts/MovingPlatform.cs
@@ -7,12 +7,16 @@ public class MovingPlatform : MonoBehaviour
     public GameObject player;
     
     private void OnCollisionEnter2D(Collision2D other) {
-         player.transform.parent = transform;
+          if(other.gameObject.CompareTag("Player")){
+               player.transform.parent = transform;
+          }
         
     }
 
     private void OnCollisionExit2D(Collision2D other) {
-         player.transform.parent = null;
+          if(other.gameObject.CompareTag("Player")){
+               player.transform.parent = null;
+          }
     }
 
 }
