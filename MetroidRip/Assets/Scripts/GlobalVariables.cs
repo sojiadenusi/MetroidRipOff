9baeda3fb74fd7f7674847195dd8b8eb3b7ceb23
Kewl <ac8736@nyu.edu>
2022-03-10 21:41:36 -0500

    update

--- MetroidRip/Assets/Scripts/GlobalVariables.cs
+++ MetroidRip/Assets/Scripts/GlobalVariables.cs
@@ -2,4 +2,6 @@ public static class GlobalVariables
 {
     public static int ammo = 20;
     public static int health = 10;
+    public static int bossHealth = 5;
+    public static int bossTentacles = 3;
 }
