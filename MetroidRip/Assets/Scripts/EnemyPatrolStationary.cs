09a268f8094a7caba7a1faebc007b23987111427
sojiadenusi <43679596+sojiadenusi@users.noreply.github.com>
2022-03-10 23:06:16 -0500

    update

--- MetroidRip/Assets/Scripts/EnemyPatrolStationary.cs
+++ MetroidRip/Assets/Scripts/EnemyPatrolStationary.cs
@@ -5,9 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class EnemyPatrolStationary : MonoBehaviour
 {
+    public AudioClip deathSound;
+    AudioSource _audioSource;
+    
+
+    private void Start() {
+        _audioSource = GetComponent<AudioSource>();
+    }
     //public string level;
     private void OnCollisionEnter2D(Collision2D other) {
         if (other.gameObject.CompareTag("Bullet")) {
+            _audioSource.PlayOneShot(deathSound);
             Destroy(this.gameObject);
         }else if (other.gameObject.CompareTag("Player")){
             int num = SceneManager.GetActiveScene().buildIndex;
